3beb177ef72ff1d06801ca92a35ecb41243a4b19
Ashish Khare <akstyles211@gmail.com>
2025-01-25 08:34:10 +0000

    submission 1

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,19 @@
 import numpy as np
 
-def calculate(list):
+def calculate(array):
+    if len(array) < 9:
+        raise ValueError("List must contain nine numbers.")
+        
+    array = np.array(array).reshape((3, 3))
 
+    def apply(func):
+        return [list(func(array, axis=0)), list(func(array, axis=1)), func(array)]
 
-
-
-    return calculations
+    return {
+        'mean': apply(np.mean),
+        'variance': apply(np.var),
+        'standard deviation': apply(np.std),
+        'max': apply(np.max),
+        'min': apply(np.min),
+        'sum': apply(np.sum),
+    }
